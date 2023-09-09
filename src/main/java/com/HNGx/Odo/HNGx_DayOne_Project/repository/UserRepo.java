6be8d37d3848f36015b100c4956006c8dc279d8b
OkechukwuOdo <okechukwuoodo@gmail.com>
2023-09-09 22:28:55 +0100

    removed git ignore

--- src/main/java/com/HNGx/Odo/HNGx_DayOne_Project/repository/UserRepo.java
+++ src/main/java/com/HNGx/Odo/HNGx_DayOne_Project/repository/UserRepo.java
@@ -2,6 +2,8 @@ package com.HNGx.Odo.HNGx_DayOne_Project.repository;
 
 import com.HNGx.Odo.HNGx_DayOne_Project.entity.UserEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
@@ -13,11 +15,11 @@ import java.util.List;
 public class UserRepo {
 
     List<UserEntity> userEntityList= List.of(
-            new UserEntity(1L,"Odo Osmond", LocalDate.now().getDayOfWeek(), Instant.now(),"Java","","",200),
-            new UserEntity(2L,"Osmond Odo", LocalDate.now().getDayOfWeek(), Instant.now(),"Node","","",200),
-            new UserEntity(3L,"Okechukwu Odo", LocalDate.now().getDayOfWeek(), Instant.now(),"Python","","",200),
-            new UserEntity(4L,"Okey Odo", LocalDate.now().getDayOfWeek(), Instant.now(),"C#","","",200),
-            new UserEntity(5L,"Odo Okechukwu", LocalDate.now().getDayOfWeek(), Instant.now(),"C++","","",200)
+            new UserEntity(1L,"Odo Osmond", LocalDate.now().getDayOfWeek(), Instant.now(),"Java","https://github.com/OkechukwuOdo/HNGx_Stage_One.git","", 200),
+            new UserEntity(2L,"Osmond Odo", LocalDate.now().getDayOfWeek(), Instant.now(),"Node","https://github.com/OkechukwuOdo/HNGx_Stage_One.git","",200),
+            new UserEntity(3L,"Okechukwu Odo", LocalDate.now().getDayOfWeek(), Instant.now(),"Python","https://github.com/OkechukwuOdo/HNGx_Stage_One.git","",200),
+            new UserEntity(4L,"Okey Odo", LocalDate.now().getDayOfWeek(), Instant.now(),"C#","https://github.com/OkechukwuOdo/HNGx_Stage_One.git","",200),
+            new UserEntity(5L,"Odo Okechukwu", LocalDate.now().getDayOfWeek(), Instant.now(),"C++","https://github.com/OkechukwuOdo/HNGx_Stage_One.git","",200)
     );
 
     public List<UserEntity> findAllUsers(){
